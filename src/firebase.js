a422d32845a31699af5e1b63da5bad3595429db7
Jia Jun <jiajunlee2@gmail.com>
2020-10-04 17:34:22 +0800

    Firebase Link added

---
+++ src/firebase.js
@@ -0,0 +1,16 @@
+import firebase from "firebase";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCyTYhdAilYNdXSgTAIXNHnbC5J6gu1Cds",
+  authDomain: "e-clinic-e8c00.firebaseapp.com",
+  databaseURL: "https://e-clinic-e8c00.firebaseio.com",
+  projectId: "e-clinic-e8c00",
+  storageBucket: "e-clinic-e8c00.appspot.com",
+  messagingSenderId: "912281579882",
+  appId: "1:912281579882:web:915439768cf00edae4b513",
+  measurementId: "G-S36ST5KXWQ",
+};
+
+firebase.initializeApp(firebaseConfig);
+var database = firebase.firestore();
+export default database;
