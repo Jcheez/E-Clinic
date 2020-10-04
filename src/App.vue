40b62fdd207e2af2f0250ffdd9bb3baa0089e62e
b-leong <bl301098@gmail.com>
2020-10-04 19:06:41 +0800

    added routes

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
     <HelloWorld msg="Welcome to E-Clinic" />
+    <router-view></router-view>
   </div>
 </template>
 
---
+++ src/routes.js
@@ -0,0 +1,7 @@
+import PendingBooking from "./components/PendingBooking.vue";
+import Test from "./components/Test.vue";
+
+export default [
+  { path: "/pendingbooking", component: PendingBooking },
+  { path: "/test", component: Test },
+];
