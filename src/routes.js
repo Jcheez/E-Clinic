79b586d68cbee264eec1b301c5888896aba0b03e
gladyskoh <66240420+gladyskoh@users.noreply.github.com>
2020-10-15 16:46:59 +0800

    Revert "Fixed Remove Slot, Upcoming Appt"

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,7 @@
 import PendingBooking from "./components/PendingBooking.vue";
 import PatientNotes from "./components/PatientNotes.vue";
-import AppointmentPage from "./components/AppointmentPage"
 
 export default [
   { path: "/pendingbooking", component: PendingBooking },
   { path: "/patientsnotes", component: PatientNotes },
-  { path: "/appointment", component: AppointmentPage },
 ];
