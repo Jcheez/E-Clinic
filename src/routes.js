ccf0b06d6683dffef6c380f53f6012c7a03ada18
Jia Jun <jiajunlee2@gmail.com>
2020-10-18 22:37:03 +0800

    Completed uploading of documents component

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,13 @@
 import PendingBooking from "./components/PendingBooking.vue";
+
+// All routes for Patient Notes Component
 import PatientNotes from "./components/PatientNotes_components/PatientNotes.vue";
 import PatientAppointments from "./components/PatientNotes_components/PatientAppointments.vue";
+import Documents from "./components/PatientNotes_components/Documents.vue"
 
 export default [
   { path: "/pendingbooking", component: PendingBooking },
   { path: "/patientsnotes", component: PatientNotes },
-  { path: "/patientsnotes/appointments", name:"appointments", props: true, component: PatientAppointments },
+  { path: "/patientsnotes/appointments", name: "appointments", props: true, component: PatientAppointments },
+  { path: "/patientsnotes/appointments/uploaddocuments", name: "uploaddocuments", props: true, component: Documents}
 ];
