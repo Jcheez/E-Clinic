d72cc8af0eeea30cb9f593f68b53591fc094137f
b-leong <bl301098@gmail.com>
2020-10-19 12:46:24 +0800

    updated makeabooking

--- src/components/MakeBookingTer.vue
+++ src/components/MakeBookingTer.vue
@@ -1,6 +1,10 @@
 <template>
   <div>
     <p>{{ msg }}</p>
+    <p>{{ msg2 }}</p>
+    <button v-on:click="home">
+      Return Home
+    </button>
   </div>
 </template>
 
@@ -8,9 +12,16 @@
 export default {
   data() {
     return {
-      msg: "Sorry your condition is not available for teleconsult",
+      msg: "Your condition is not available for teleconsult / You have not been to this clinic",
+      msg2: "Clinic staff will contact you to make arrangements"
     };
   },
+  methods: {
+    home: function () {
+      // change to the route for the patient page after login component is complete
+      this.$router.push('/')
+    }
+  }
 };
 </script>
 
