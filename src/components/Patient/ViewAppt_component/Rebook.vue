0618c4226b8e91350f00c469fc4a3d605a764c18
b-leong <bl301098@gmail.com>
2020-11-02 15:13:18 +0800

    changes to reschedule

--- src/components/Patient/ViewAppt_component/Rebook.vue
+++ src/components/Patient/ViewAppt_component/Rebook.vue
@@ -1,17 +1,15 @@
 <template>
   <div>
-      <h1>{{ msg }}</h1>
-      <v-date-picker v-model="date" is-inline :min-date="new Date()" id="datepicker"/>
-      <ul id="slots">
-      <li v-for="(s, index) in this.slot" :key="index">
+    <p>{{ msg }}</p>
+    <v-date-picker v-model="date" is-inline :min-date="new Date()" id="datepicker"/>
+    <ul id="slots">
+      <li v-for="(s, index) in this.docsName" :key="index">
         <div id="inner">
-          <span>Date: {{  formatDate(s.date) }}</span>
-          <span>Time: {{  formatTime(s.date) }}</span>
-          <span>Doctor: {{ s.doctor }}</span>
+          <h3 style="font-size:30px; text-decoration: underline;">{{s}}</h3>
+            <div v-for="(v, index) in slot" :key="index">
+              <button v-if="s.localeCompare(v.doctorName) == 0" v-on:click="getdoc(v.id); updateApptHist();">{{formatTime(v.date)}}</button>
+            </div>
         </div>
-        <button id="view" v-on:click="getdoc(s.id); changeBooking(s.id); updateApptHist();">
-          Make Booking
-        </button>
       </li>
     </ul>
   </div>
@@ -27,7 +25,8 @@ export default {
         msg: "Reschedule Appointment",
         date: new Date(),
         slot: [],
-        datadoc: {}
+        datadoc: {},
+        docsName: [],
     };
   },
 
@@ -35,21 +34,48 @@ export default {
       fetchitems: function() {
           database
           .collection("consultslots")
+          .orderBy("date")
           .get()
           .then((querySnapShot) => {
               this.slot = []
+              this.docsName = []
               let item = {};
             querySnapShot.forEach((doc) => {
                 item = doc.data();
-                let item_date = item.date.toDate().toLocaleDateString().split("/").reverse().join("-");
-                let filtered_date = this.date.toLocaleDateString().split("/").reverse().join("-")
-                if (item_date.localeCompare(filtered_date) == 0 && item.patient == null) {
-                    item.id = doc.id;
-                    this.slot.push(item);
-                }
+                let item_date = item.date
+                .toDate()
+                .toLocaleDateString()
+                .split("/")
+                .reverse()
+                .join("-");
+                
+                let filtered_date = this.date.toLocaleDateString()
+                .split("/")
+                .reverse()
+                .join("-")
+
+                if (item_date.localeCompare(filtered_date) == 0 && item.patient == null && item.clinic.localeCompare(this.clinic) == 0) {
+                  let item2 = item
+                  item2.id = doc.id;
+                  database
+                  .collection("doctors")
+                  .doc(item.doctor)
+                  .get()
+                  .then((doc) => {
+                    let docName = doc.data().name;
+                    console.log(docName)
+                    item2.doctorName = docName
+                    console.log(item)
+                    this.slot.push(item2);
+                    if (this.docsName.includes(docName) == false){
+                      this.docsName.push(docName)
+                    }
+                    });
+
+                    }
+                });
             });
-          });
-      },
+        },
 
       formatDate: function(date) {
           let ldate = date.toDate().toLocaleDateString().split("/")
@@ -79,6 +105,26 @@ export default {
               querySnapShot.forEach((doc) => {
                   if (ide.localeCompare(doc.id) == 0) {
                       this.datadoc = doc.data()
+                      database
+                      .collection("consultslots")
+                      .doc(ide)
+                      .update({
+                          patient: this.patientId,
+                          conditions: this.conditions
+                      })
+                      .then(() => {
+                      database.collection('doctors').doc(this.datadoc.doctor).get().then((doc) => {
+                        this.$router.push({
+                          name: "makebookingconfirmation",
+                          params: {
+                              appdate: this.datadoc.date,
+                              doctor: doc.data().name,
+                          }   
+                          })
+                        })
+                      alert("Appointment Slot booked")
+                      })
+                      
                   }
               })
            })
@@ -174,59 +220,26 @@ export default {
 </script>
 
 <style scoped>
-div#inner {
-  width: 70%;
-  display: inline-block;
-}
-
-ul#slots {
-    position:absolute;
-    left: 500px;
-    top:-100px
-}
-
-li {
-  position: relative;
-  width: 562px;
-  height: 100px;
-  left: 73px;
-  top: 300px;
-  border: 1px solid #000000;
-  box-sizing: border-box;
-  list-style-type: none; /* Remove bullets */
-  padding-left: 10px;
-  padding-top: 20px;
-  display: block;
-}
-
-span {
-  position: relative;
-  display: block;
-  text-align: left;
-  font-size: 20px;
-}
-
-button#view {
-  position: relative;
-  bottom: 35px;
-  width: 125px;
-  height: 50px;
-  background: aqua;
-  border: 1px solid #000000;
-  box-sizing: border-box;
-  border-radius: 15px;
-  font-size: 20px;
-}
-
-button:hover {
-  cursor: pointer;
-}
-button:focus {
-  outline: none;
-}
-
 #datepicker {
     position: absolute;
     left: 200px;
+}
+
+button {
+  transition: box-shadow 0.3s;
+  transition: 0.3s;
+  color: rgb(0, 114, 180);
+  letter-spacing: 2px;
+  width: 125px;
+  height: 45px;
+  background-color: white;
+  border: 1px solid rgb(0, 114, 180);
+  border-radius: 5px;
+  z-index: -1;
+  cursor: pointer;
+}
+
+li {
+  list-style-type: none; /* Remove bullets */
 }
 </style>
