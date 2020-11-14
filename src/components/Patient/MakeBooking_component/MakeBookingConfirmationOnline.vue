f015a9c35716c67ff9a4ba9bb83c0f78ab4860e0
Mochihaha <loh.ziying@gmail.com>
2020-11-15 02:58:16 +0800

    add topNavBar

--- src/components/Patient/MakeBooking_component/MakeBookingConfirmationOnline.vue
+++ src/components/Patient/MakeBooking_component/MakeBookingConfirmationOnline.vue
@@ -1,5 +1,10 @@
 <template>
   <div>
+    <div id="topNavBar">
+      <span>E-Clinic</span>
+      <router-link to="/patienthome">Return Home</router-link>
+      <router-link to="/pending">Pending Booking</router-link>
+    </div>
     <h4 id="thanks">Thank You</h4>
     <h4 id="patientName">{{ this.getUser.displayName }}</h4>
     <h4 id="confirm">Here Are the Confirmation Details of Your Booking</h4>
@@ -9,7 +14,9 @@
       <p>Appointment Time: {{ formatTime(appdate) }}</p>
       <p>Doctor: {{ doctor }}</p>
       <p>Consultation type: Online</p>
-      <p>You may want to print this page as proof of booking confirmation</p>
+      <p id="print">
+        You may want to print this page as proof of booking confirmation
+      </p>
     </div>
   </div>
 </template>
@@ -59,9 +66,50 @@ export default {
 </script>
 
 <style scoped>
+#topNavBar span {
+  font-family: Nunito;
+  font-size: 24px;
+  letter-spacing: 4px;
+  color: white;
+  font-weight: bolder;
+  width: 100%;
+  display: block;
+}
+#topNavBar a {
+  color: rgb(238, 249, 255);
+  transition: 0.3s;
+  font-family: Nunito;
+  font-size: 17px;
+  letter-spacing: 2px;
+  margin: 10px 50px 0 75px;
+  text-decoration: none;
+  font-weight: bold;
+  display: inline-block;
+}
+
+#topNavBar a:hover {
+  font-size: 18px;
+  color: white;
+  cursor: pointer;
+}
+
+#topNavBar {
+  height: 80px;
+  position: fixed;
+  padding-top: 20px;
+  top: 0;
+  left: 0;
+  overflow-x: hidden;
+  width: 100%;
+  /* border: 1px solid white; */
+  /* border-radius: 5px; */
+  background-color: rgb(0, 114, 180);
+  color: rgb(238, 249, 255);
+}
+
 #confirm {
   position: absolute;
-  left: 300px;
+  left: 270px;
   font-family: Nunito;
   font-size: 32px;
   top: 160px;
@@ -70,7 +118,7 @@ export default {
   font-family: Nunito;
   font-size: 32px;
   position: absolute;
-  left: 500px;
+  left: 470px;
   top: 100px;
 }
 #patientName {
@@ -78,16 +126,23 @@ export default {
   font-family: Nunito;
   font-size: 32px;
   position: absolute;
-  left: 680px;
+  left: 650px;
   top: 100px;
 }
 p {
   font-size: 20px;
   font-family: Nunito;
 }
+#print {
+  font-size: 16px;
+  position: absolute;
+  left: -50px;
+}
 #details {
   position: absolute;
   top: 250px;
-  left: 300px;
+  left: 470px;
+  text-align: left;
+  width: 500px;
 }
 </style>
